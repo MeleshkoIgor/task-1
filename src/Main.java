3d4a7319b6f70cf609ed6d972f2e9b17c6aaf299
Igor Meleshko <igor.meleshko.1109@gmail.com>
2021-10-10 01:39:35 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentSum = 2_000_000_000;
+        int transferSum = 500_000_000;
+        int totalSum = currentSum + transferSum;
+        System.out.println(totalSum);
+    }
+}
